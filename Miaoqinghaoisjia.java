2d822654396390d319f66d146e3c5cebc93235ed
wuheng <921134156@qq.com>
2020-09-08 11:27:50 +0800

    公司提交

--- Miaoqinghaoisjia.java
+++ Miaoqinghaoisjia.java
@@ -1,13 +1,8 @@
 public class Miaoqinghaoisjia{
-<<<<<<< HEAD
    private String name;
    private int age;
    private int phone;
    public void miao(){
    
    }
-
-=======
-    private int age;
->>>>>>> 在公司开发第一次
 }
